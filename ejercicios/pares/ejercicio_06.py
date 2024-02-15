81a083d856626048598a57ed68a175be5247d2d8
hola0511 <espinosaariassamuel@gmail.com>
2024-02-15 18:25:51 -0500

    punto 6 listo

---
+++ ejercicios/pares/ejercicio_06.py
@@ -0,0 +1,11 @@
+lista = [8, 25, 122, 47, 1050]
+
+def suma_lista(lista):
+    contador = 0
+    for i in lista:
+        contador += i
+    return contador
+
+print(suma_lista(lista))
+
+## cantidad esperada de la lista actual es 1252
